4baf1227251132ef8a5660e8b72886e92791ede4
nata-med <natalam59163@gmail.com>
2026-05-25 19:19:19 +0700

    Add files via upload

--- assembly.sh
+++ assembly.sh
@@ -1,4 +1,4 @@
-gcc main.c help.c utility.c modification_commands.c reading_analytics_commands.c -o mygit.exe
+gcc main.c help.c utility.c modification_commands.c reading_analytics_commands.c errors.c -o mygit
 
 # ==========================================
 # ПРИМЕР ИСПОЛЬЗОВАНИЯ ВСЕХ КОМАНД 
@@ -8,20 +8,20 @@ gcc main.c help.c utility.c modification_commands.c reading_analytics_commands.c
 # ./mygit init
 #
 # 2. Работа с файлами (создайте test1.txt и test2.txt вручную)
-# ./mygit add test1.
+# ./mygit add test1.txt
 # ./mygit add test2.txt
 # ./mygit status
 # ./mygit remove test2.txt
 # ./mygit status
 #
 # 3. Сохранение (Коммит)
-# ./mygit commit "first_commit"
+# ./mygit commit "first commit"
 #
 # 4. Аналитика
 # ./mygit log (выводит цепочку всех коммитов)
 # ./mygit log --n 1
 # (измените test1.txt руками)
-# ./mygit diff (должны вывестись отлия от прошлого файла построчно)
+# ./mygit diff (должны вывестись отличия от прошлого файла построчно)
 #
 # 5. Изменение существующего
 # (скопируйте хэш коммита из log)
